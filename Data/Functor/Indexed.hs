0680ce7140019c355f7792f232ea01d5a1cd031d
Zie Weaver <zearen.wover@hotmail.com>
2016-07-21 09:06:29 -0700

    Add Applicative-style operators for IxApplicative.

--- Data/Functor/Indexed.hs
+++ Data/Functor/Indexed.hs
@@ -14,6 +14,7 @@ module Data.Functor.Indexed
   , IxCopointed(..)
   , IxPointed(..)
   , IxApplicative(..)
+  , (<<*>>), (<<*), (*>>)
   ) where
 
 class IxFunctor f where
@@ -21,6 +22,21 @@ class IxFunctor f where
 
 class IxPointed m => IxApplicative m where
   iap :: m i j (a -> b) -> m j k a -> m i k b
+
+-- | Infix alias of 'iap'.  Or, ('<*>') for 'IxApplicative'.
+infixl 4 <<*>>
+(<<*>>) :: IxApplicative f => f i j (a -> b) -> f j k a -> f i k b
+(<<*>>) = iap
+
+-- | ('Control.Applicative.<*') for 'IxApplicative'.
+infixl 4 <<*
+(<<*) :: IxApplicative f => f i j a -> f j k b -> f i k a
+(<<*) a b = imap const a <<*>> b
+
+-- | ('Control.Applicative.*>') for 'IxApplicative'.
+infixl 4 *>>
+(*>>) :: IxApplicative f => f i j a -> f j k b -> f i k b
+(*>>) a b = imap (const id) a <<*>> b
 
 class IxFunctor m => IxPointed m where
   ireturn :: a -> m i i a
